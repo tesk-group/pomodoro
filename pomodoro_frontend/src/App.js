8a7e5a936715f25f0073ee6864f9d1249b5d841f
kirkknutsen <eevee.gg@gmail.com>
2023-04-18 03:27:53 -0400

    Timer Component Added

--- pomodoro_frontend/src/App.js
+++ pomodoro_frontend/src/App.js
@@ -1,18 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+const renderTime = ({ remainingTime }) => {
+  if (remainingTime === 0) {
+    return <div className="timer">Pomodoro Over</div>;
+  }
+
+  return (
+    <div className="timer">
+      <div className="value">{remainingTime}</div>
+      <div className="text">seconds left in Pomodoro </div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <div className="timer-wrapper">
+        <CountdownCircleTimer
+          isPlaying
+          duration={100}
+          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+          colorsTime={[10, 6, 3, 0]}
+          onComplete={() => ({ shouldRepeat: false, delay: 1 })}
+        >
+          {renderTime}
+        </CountdownCircleTimer>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
