c5fb86e05f17b8874babac88ff010c5bd4acc83d
Elsa <elsa.glz.aguilar@hotmail.com>
2023-04-22 21:55:42 -0400

    Added laravel template

---
+++ pomodoro_backend/database/migrations/2023_04_22_212780_add_timer_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('timers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreignId('task_id')->nullable();
+            $table->string('status');
+            $table->string('type')->nullable();
+            $table->timestamp('end_time');
+            $table->timestamp('interrupted_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('timers');
+    }
+};
